cf37090306b15a68e9ef6aa5f172fbe1f09f35f6
gw8080 <52276914+gw8080@users.noreply.github.com>
2022-07-22 17:46:40 +1000

    Update budget.py

--- budget.py
+++ budget.py
@@ -1,6 +1,6 @@
 from numpy import random
 print("Financial indecision calculator")
-print("A three-sided die is impossible. nevertheless, throwing the three-sided die over X amount of time will give you the decision making ability you need. Sooner or later the object of desire will be chosen, thus making uniform distribution an ideal tool for budgeting.")
+print("A three-sided die is impossible. nevertheless, throwing the three-sided die over X amount of time will give you the decision making ability you need. Sooner or later the object of desire will be chosen, otherwise a balance of other selections will be generated, thus making uniform distribution an ideal tool for budgeting.")
 while(True):
     varA = int(input("Choice count: "))
     x = random.uniform(0, varA, size=(1, varA))[0]
